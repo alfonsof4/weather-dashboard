195677c88d12e807ae1d4ea2f80c05b780c56245
alfonsof4 <alfonsofernandez4th@gmail.com>
2021-12-12 23:44:33 -0600

    final touches

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# weather-dashboard
+# Weather Dashboard
+## Description
+The following website gives a user the current
+weather based on which city they type in the 
+search box.
+
+## Built With
+* HTML
+* CSS
+* JavaScript
+
+## Website:
+
+## Contribution
+Alfonso Fernandez 
--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,83 @@
-const api = {
-    key: "04cb38649b0f668118769eba09f6a313",
-    baseurl: "https://api.openweathermap.org/data/2.5"
-}
+const form = document.querySelector(".top-banner form");
+const input = document.querySelector(".top-banner input");
+const msg = document.querySelector(".top-banner .msg");
+const list = document.querySelector(".weather-display .cities");
 
-const searchbar = document.querySelector('.search-bar');
-searchbar.addEventListener('keypress', setQuery);
+const apiKey = "ce2e0bbc1a4e7d4f889b0776c6f917d8";
 
-function setQuery(evt) {
-    if (evt.keyCode == 13) {
-        //getResults(searchbar.value);
-        console.log(searchbar.value);
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  let inputVal = input.value;
+
+  
+  const listItems = list.querySelectorAll(".weather-display .city");
+  const listItemsArray = Array.from(listItems);
+
+  if (listItemsArray.length > 0) {
+    const filteredArray = listItemsArray.filter(el => {
+      let content = "";
+      
+      if (inputVal.includes(",")) {
+        
+        if (inputVal.split(",")[1].length > 2) {
+          inputVal = inputVal.split(",")[0];
+          content = el
+            .querySelector(".city-name span")
+            .textContent.toLowerCase();
+        } else {
+          content = el.querySelector(".city-name").dataset.name.toLowerCase();
+        }
+      } else {
+        
+        content = el.querySelector(".city-name span").textContent.toLowerCase();
+      }
+      return content == inputVal.toLowerCase();
+    });
+
+    if (filteredArray.length > 0) {
+      msg.textContent = `You already know the weather for ${
+        filteredArray[0].querySelector(".city-name span").textContent
+      }`;
+      form.reset();
+      input.focus();
+      return;
     }
-}
+  }
+
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const { main, name, sys, weather } = data;
+      const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${
+        weather[0]["icon"]
+      }.svg`;
+
+      const li = document.createElement("li");
+      li.classList.add("city");
+      const markup = `
+        <h2 class="city-name" data-name="${name},${sys.country}">
+          <span>${name}</span>
+          <sup>${sys.country}</sup>
+        </h2>
+        <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+        <figure>
+          <img class="city-icon" src="${icon}" alt="${
+        weather[0]["description"]
+      }">
+          <figcaption>${weather[0]["description"]}</figcaption>
+        </figure>
+      `;
+      li.innerHTML = markup;
+      list.appendChild(li);
+    })
+    .catch(() => {
+      msg.textContent = "Please search for a valid city";
+    });
+
+  msg.textContent = "";
+  form.reset();
+  input.focus();
+});
